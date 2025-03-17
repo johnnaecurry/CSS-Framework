1d6c82f1e4b22aec3eba52d3e244e0ede3ec02b2
Johnnae Curry <curryjo1@msu.edu>
2025-03-17 00:43:06 +0000

    java

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Add JavaScript below
+<script type="module">
+  import { Toast } from 'bootstrap.esm.min.js'
+
+  Array.from(document.querySelectorAll('.toast'))
+    .forEach(toastNode = new Toast(toastNode))
+</script>
